b6a7041b4ac450402c692250cd67aeb01e688a9b
mattchan8 <mchan8@buffalo.edu>
2023-12-28 23:58:07 -0500

    Update versusGame.py
    included timeouts for detecting iframe content

--- src/versusGame.py
+++ src/versusGame.py
@@ -10,29 +10,44 @@ class VersusGame:
         self.webdriver = browser.webdriver
 
     def completeVersusGame(self):
-        self.webdriver.get("https://www.bing.com/?form=ML2NF7&rwgbopen=1&rwgbscenario=versus&thirdPartyPartnerId=VersusGame")
-        time.sleep(10)
-        flyout = self.webdriver.execute_script('return document.querySelector("#panelFlyout")')
-        print(flyout)
-        while (flyout is None):
-            flyout = self.webdriver.execute_script('return document.querySelector("#panelFlyout")')
-            print(f'looping flyout {flyout}')
-            time.sleep(5)
-        time.sleep(10)
-        panelFlyoutLink = self.webdriver.execute_script('return document.getElementById("panelFlyout").src')
-        self.webdriver.get(panelFlyoutLink)
-        time.sleep(10)
-
-        versusIframe = self.webdriver.execute_script('return document.querySelector("#versusIframe")')
-        while (versusIframe is None):
-            versusIframe = self.webdriver.execute_script('return document.querySelector("#versusIframe")')
-            print(f'looping veruses iframe{versusIframe}')
-            time.sleep(5)
-        versusGameLink = self.webdriver.execute_script('return document.querySelector("#versusIframe").src')
-        self.webdriver.get(versusGameLink)
-        time.sleep(10)
-
-        for _ in range(3):
-            self.webdriver.execute_script("document.querySelectorAll('button')[1].click()")
+        try:
+            i = 0
+            self.webdriver.get("https://www.bing.com/?form=ML2NF7&rwgbopen=1&rwgbscenario=versus&thirdPartyPartnerId=VersusGame")
             time.sleep(10)
-        logging.info("[Versus Game] Completed Versus Game successfully!")
+            flyout = self.webdriver.execute_script('return document.querySelector("#panelFlyout")')
+            print(flyout)
+            while (flyout is None):
+                flyout = self.webdriver.execute_script('return document.querySelector("#panelFlyout")')
+                print(f'looping flyout {flyout}')
+                time.sleep(10)
+                i += 1
+                if i == 5:
+                    logging.info("[Versus Game] Versus Game Failed!")
+                    return
+            i = 0
+            time.sleep(10)
+            panelFlyoutLink = self.webdriver.execute_script('return document.getElementById("panelFlyout").src')
+            self.webdriver.get(panelFlyoutLink)
+            time.sleep(10)
+
+            versusIframe = self.webdriver.execute_script('return document.querySelector("#versusIframe")')
+            while (versusIframe is None):
+                versusIframe = self.webdriver.execute_script('return document.querySelector("#versusIframe")')
+                print(f'looping veruses iframe{versusIframe}')
+                time.sleep(5)
+                i += 1
+                if i == 5:
+                    logging.info("[Versus Game] Versus Game Failed!")
+                    return
+            i = 0
+            versusGameLink = self.webdriver.execute_script('return document.querySelector("#versusIframe").src')
+            self.webdriver.get(versusGameLink)
+            time.sleep(10)
+
+            for _ in range(3):
+                self.webdriver.execute_script("document.querySelectorAll('button')[1].click()")
+                time.sleep(10)
+            logging.info("[Versus Game] Completed Versus Game successfully!")
+        except:
+            logging.info("[Versus Game] Versus Game Failed!")
+            pass	
